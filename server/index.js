1275af4951ae9be217c8ac30b9413ca1eac71cc2
Praveen Kumar <praveengopi16@gmail.com>
2020-11-09 20:21:45 +0530

    Added callback and token api

--- server/index.js
+++ server/index.js
@@ -3,22 +3,105 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const dotenv = require('dotenv')
 const app = express()
+const axios = require('axios')
+const uuid = require('uuid')
+dotenv.config()
 
-app.use(bodyParser.urlencoded({ extended: true })) //check
-app.use(bodyParser.json()) //check
+app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(bodyParser.json()) 
 app.use(cookieParser())
 app.use(cors())
 
+let client_id = process.env.CLIENT_ID
+let client_secret = process.env.CLIENT_SECRET
+
+let redirect_uri = 'http://localhost:5000/callback'
+let frontend_uri = 'http://localhost:3000'
+let statekey = 'spotify_auth_state'
+
+let tokensForAuth ={}
+
+const generateRandomString = function (length) {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (let i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+};
+
 app.get('/login', function(req, res) {
-var scopes = 'user-read-private user-read-email';
-res.redirect('https://accounts.spotify.com/authorize' +
-  '?response_type=code' +
-  '&client_id=' + my_client_id +
-  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  '&redirect_uri=' + encodeURIComponent(redirect_uri));
+
+	let scopes = 'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-read-currently-playing user-library-read playlist-read-private playlist-read-collaborative';
+
+	const state = generateRandomString(16);
+
+	res.cookie(statekey,state)
+
+	res.redirect('https://accounts.spotify.com/authorize' +
+		'?response_type=code' + '&client_id=' + client_id +
+		(scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+		'&redirect_uri=' + encodeURIComponent(redirect_uri) + 
+		'&state='+state);
 });
 
+
+app.get('/callback', async function(req,res){
+	try{
+		let recievedState = req.query.state || null
+		let receivedCode = req.query.code || null
+		let statecookie = req.cookies?req.cookies[statekey]:null
+
+		if(recievedState === null || recievedState !== statecookie) {
+			console.log("invalid state")
+			return res.redirect('http://localhost:3000?token=invalid')
+		}
+		else{
+			const params = new URLSearchParams();
+			params.append('code', receivedCode);
+			params.append('redirect_uri', redirect_uri);
+			params.append('grant_type',  'authorization_code');
+
+			const {data}  = await axios({
+				method:'POST',
+				url:'https://accounts.spotify.com/api/token',
+				params,
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					Authorization: `Basic ${new Buffer.from(`${client_id}:${client_secret}`).toString(
+						'base64',
+						)}`,
+				},
+				json:true
+			})
+			data.recivedAt = Date.now()
+			let tempTokenId = uuid.v4()
+			tokensForAuth[tempTokenId]=data
+			
+			return res.redirect(`http://localhost:3000?token=${tempTokenId}`)
+		}
+	}
+	catch(e)
+	{
+		return console.log(e)
+	}
+
+
+});
+
+
+app.put('/getAuthToken', function(req,res){
+	let tempTokenId = req.query.token || null
+	if(tempTokenId){
+		return res.status(200).json(tokensForAuth[tempTokenId])
+	}
+	return res.json({error:"Code not found"})
+})
+
+
 app.listen(5000, () => {
-    console.log("server is running on port 5000")
+	console.log("server is running on port 5000")
 })
